049085400e78e68b9c2198cf4b34563e2bb2003e
sunethra <sunethra.duvvuru1@t-mobile.com>
2021-09-21 16:47:52 -0500

    Intitial code commit

---
+++ src/main/java/com/adp/sample/demo/UserChangeCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.adp.sample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserChangeCalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserChangeCalculatorApiApplication.class, args);
+	}
+
+}
